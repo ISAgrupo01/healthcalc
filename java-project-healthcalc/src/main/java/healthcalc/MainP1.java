baaf8833a99120ece7d08861dff97abdc20a2a1c
Sabrina <sabriinael160@gmail.com>
2026-05-13 19:43:32 +0200

    feat(adapter): implementar patrón Adapter para HealthHospital

--- java-project-healthcalc/src/main/java/healthcalc/MainP1.java
+++ java-project-healthcalc/src/main/java/healthcalc/MainP1.java
@@ -12,6 +12,7 @@ public class MainP1 {
         System.out.println("1. Calcular BMI (Índice de Masa Corporal) - Versión FULL");
         System.out.println("2. Calcular MAP (Presión Arterial Media)");
         System.out.println("3. Calculadora de IBW (Peso Ideal)");
+        System.out.println("4. Prueba Adaptador Hospital Costa del Sol de Marbella");
         System.out.print("Selecciona una opción: ");
 
         int opcion = scanner.nextInt();
@@ -56,9 +57,34 @@ public class MainP1 {
                 String sexo = scanner.next().toLowerCase(); 
                 double ibw = calculator.ibw(altura, sexo);
                 System.out.println("Su IBW es: " + ibw + " kg");
+            
+            } else if (opcion == 4) {
+                //PRUEBA PATRÓN ADAPTER
+                System.out.println("\n--- SISTEMA HOSPITAL COSTA DEL SOL DE MARBELLA---");
+                HealthHospital hospitalSystem = new HealthHospitalAdapter();
+
+                System.out.print("Introduce altura (m): ");
+                float altura = scanner.nextFloat();
+
+                System.out.print("Introduce peso (g): ");
+                int pesoGramos = scanner.nextInt();
+
+                System.out.print("Introduce género ('M' para Mujer, 'H' para Hombre): ");
+                char genero = scanner.next().charAt(0);
+
+                //Probar Índice de Masa Corporal (BMI) con la interfaz del hospital
+                Tuple<Float, String> resultadoBMI = hospitalSystem.indiceMasaCorporal(altura, pesoGramos);
+                System.out.println("\n--- RESULTADOS HOSPITAL ---");
+                System.out.printf("Su Índice de Masa Corporal es: %.2f\n", resultadoBMI.getX());
+                System.out.println("Clasificación: " + resultadoBMI.getY());
+
+                //Probar Peso Corporal Ideal con la interfaz del hospital
+                int resultadoIBW = hospitalSystem.pesoCorporalIdeal(genero, altura);
+                System.out.println("Su Peso Corporal Ideal es: " + resultadoIBW + " kg");
+
             } else {
                 System.out.println("Opción no válida.");
-            }
+            }    
 
         } catch (InvalidHealthDataException e) {
             System.err.println("\nERROR DE DATOS: " + e.getMessage());
